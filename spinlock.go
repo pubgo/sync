062079b14a37d7ceac96a96553f0c9cf42d65a6b
百里(Barry) <kooksee@163.com>
2020-07-15 08:55:23 +0800

    fix: add try lock

--- spinlock.go
+++ spinlock.go
@@ -1,12 +1,29 @@
 package sync
 
 import (
+	"errors"
 	"runtime"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 type spinLock uint32
+
+func (t *spinLock) TryLock(dur time.Duration) error {
+	tk := time.Tick(dur)
+	for {
+		select {
+		case <-tk:
+			return errors.New("timeout")
+		default:
+			if atomic.CompareAndSwapUint32((*uint32)(t), 0, 1) {
+				return nil
+			}
+			runtime.Gosched()
+		}
+	}
+}
 
 func (t *spinLock) Lock() {
 	for !atomic.CompareAndSwapUint32((*uint32)(t), 0, 1) {
